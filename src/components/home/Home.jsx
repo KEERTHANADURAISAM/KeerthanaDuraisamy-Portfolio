a96cd557209bcbd3e5debb6aff2cf0c2a6d09469
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2024-09-21 22:56:34 +0530

    router

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import '../home/Home.scss'
 import home from '../navbar/img/14284d_2cb43613a8c74caf8e18755236501525~mv2.gif'
 import scroll from '../home/scroll.png'
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 
 
@@ -60,8 +61,8 @@ const Home = () => {
       <motion.h2 variants={textVariants}>Keerthana Duraisamy</motion.h2>
       <motion.h1 variants={textVariants}>Mern Stack Developer</motion.h1>
       <motion.div className="home-btns">
-        <motion.button variants={textVariants}>See the Latest Works</motion.button>
-        <motion.button variants={textVariants}>Contact Me</motion.button>
+              <motion.button variants={textVariants}>See the Latest Works</motion.button>
+              <motion.button variants={textVariants}>Contact Me</motion.button>
       </motion.div>
       <motion.img variants={textVariants} animate="scrollButton" src={scroll} alt='scroll' className='scroll-img'/>
      </motion.div>
